dd5c91baa6873abaf705c53a52bd35bbdf6ce4f3
dc820 <dc820@scarletmail.rutgers.edu>
2016-11-08 19:20:38 -0500

    Created Card Subclass (CardProspector)

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -16,13 +16,28 @@ public class Prospector : MonoBehaviour {
 		S = this;
 	}
 
-	void Start() {
+    public List<CardProspector> drawPile;
+
+    void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
 
         layout = GetComponent<Layout>(); // Get the Layout
         layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector; // 1
+            lCP.Add(tCP);
+        }
+        return (lCP);
     }
 
 }
